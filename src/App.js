0d92073805fc24d099118ba40d696d0d4fdd9025
Creatiweco <creatiweco07@gmail.com>
2025-03-21 21:02:03 +0300

    app.js update 2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Navbar from "./Components/Navbar";
 import Home from "./Pages/Home";
